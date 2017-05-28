eefc62b45af22127036e1544b90fcb7afdaed4e7
Hussain <kawzaki@yahoo.com>
2017-05-28 13:05:16 +0300

    updated the menu and created product sample to be added.

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).foundation();
+
+// autocomplete UI 
+$( document ).ready(function() {	
+	$.getJSON( 'search/autosuggest/?callback=1', function( data ) {
+		automplete_cache = $.map(data, function(el) { return el; })
+		
+		$( "#searchTerms" ).autocomplete({
+		  autoFocus: true,
+		  appendTo: $(".ui-front"),
+		  minLength: 2,
+		  //source: automplete_cache
+		  source: function(request, response) {
+			  var results = $.ui.autocomplete.filter(automplete_cache, request.term);
+			  response(results.slice(0, 10));
+			  }		  
+		});
+	});
+	
+});
+
